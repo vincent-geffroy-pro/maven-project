b59caec03b9f21c86ec16ec7a1ace665e184d763
tahararib <37878662+tahararib@users.noreply.github.com>
2024-07-16 09:52:41 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,6 +16,10 @@ public class Greeter {
    // contructeur par défaut
   }
 
+   public void doSomeThing() {
+   // do no thing yet !
+  }
+ 
  /**
  * @param someone name of a person
  * @return greeting string
